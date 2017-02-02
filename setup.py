d51f9d05ce9f86c502822759dae1edd6996b1d5a
Jonas Baumann <jone@jone.ch>
2017-02-02 10:11:33 +0100

    Drop Plone 4.1 support.

--- setup.py
+++ setup.py
@@ -21,7 +21,6 @@ setup(name='collective.editmodeswitcher',
       # http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         'Framework :: Plone',
-        'Framework :: Plone :: 4.1',
         'Framework :: Plone :: 4.2',
         'Framework :: Plone :: 4.3',
         'Intended Audience :: Developers',
